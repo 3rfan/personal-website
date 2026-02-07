5faddbbf843f6a900208cc1a921976485747bce3
Erfan Hosseini <erfan_hosseini@hotmail.com>
2026-02-07 22:43:08 +0100

    fix: missing website icon configs

--- astro.config.ts
+++ astro.config.ts
@@ -2,15 +2,10 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
-// https://astro.build/config
 export default defineConfig({
-  site: process.env.CI
-    ? 'https://astro-shadcn-ui-template.vercel.app'
-    : 'http://localhost:4321',
+  site: import.meta.env.PROD ? 'https://e-hosseini.com' : 'http://localhost:4321',
   integrations: [
     react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
+    tailwind({ applyBaseStyles: false }),
   ],
 })
